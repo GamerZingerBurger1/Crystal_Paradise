2511647139e6934e433a4550b86c2e240a1f3f17
gamer <jupiterinc3@gmail.com>
2023-04-08 22:24:03 +0100

    Fixed bug
    Help from solengelo

--- src/main/java/com/gamer/CrystalParadise/CrystalParadise.java
+++ src/main/java/com/gamer/CrystalParadise/CrystalParadise.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gamer.CrystalParadise;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,14 +19,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("CrystalParadise.MODID")
+@Mod(CrystalParadise.MODID)
 public class CrystalParadise
 {
-        public static final String MODID = "cp";
+        public static final String MODID = "crystalparadise";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public CrystalParadise() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -55,7 +55,7 @@ public class CrystalParadise
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("crystalparadise", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
