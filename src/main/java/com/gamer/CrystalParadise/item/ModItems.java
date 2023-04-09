bb2b64bb858d2c97b5c8838db02c9a018b4734a4
gamer <jupiterinc3@gmail.com>
2023-04-09 03:23:00 +0100

    added OJade Item

---
+++ src/main/java/com/gamer/CrystalParadise/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.gamer.CrystalParadise.item;
+
+import com.gamer.CrystalParadise.CrystalParadise;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static net.minecraftforge.registries.ForgeRegistries.Keys.ITEMS;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CrystalParadise.MODID);
+
+    public static final RegistryObject<Item> OJADE_PINK = ITEMS.register("ojade_pink.json",
+            () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ModItems.register(eventBus);
+    }
+
+}
